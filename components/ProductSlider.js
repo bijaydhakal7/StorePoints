dca203601e99b562defe51735ff9b5e425a7f1c4
Bijay Dhakal <127229623+bijaydhakal7@users.noreply.github.com>
2025-10-28 23:19:08 +0545

    Remove unused Swiper CSS imports
    Removed unused Swiper CSS imports from ProductSlider.js.

--- components/ProductSlider.js
+++ components/ProductSlider.js
@@ -2,9 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+
 import Link from "next/link";
 const ProductSlider = () => {
   const [products, setProducts] = useState([]);
